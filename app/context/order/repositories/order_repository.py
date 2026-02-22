c3c5872cb2421e98692c3d7a8899cbd162bb224c
Alina <beketova2007@bk.ru>
2026-02-22 17:37:54 +0300

    add methods order

--- app/context/order/repositories/order_repository.py
+++ app/context/order/repositories/order_repository.py
@@ -1,8 +1,42 @@
+from sqlalchemy import select
+
 from app.base.repositories.base_repository import BaseRepository
 from app.context.order.models.order_model import OrderModel
-from app.context.order.schemas.order_schema import CreateOrderDTO, OrderDTO, UpdateOrderDTO
+from app.context.order.schemas.order_schema import CreateOrderDTO, OrderDTO, UpdateOrderDTO, UserOrderDTO
+from app.context.status.models.status_model import StatusModel
+from app.context.user.models.user_model import UserModel
 
 
 class OrderRepository(BaseRepository[CreateOrderDTO, UpdateOrderDTO, OrderDTO]):
     dto = OrderDTO
     model = OrderModel
+    user = UserModel
+    status = StatusModel
+
+    async def get_user_order_by_email(self, user_email: str) -> list[UserOrderDTO]:
+        return [
+            UserOrderDTO(**order)
+            for order in (
+                (
+                    await self._session.execute(
+                        select(
+                            self.model.order_number,
+                            self.model.total_amount,
+                            self.model.discount_amount,
+                            self.model.final_amount,
+                            self.model.delivery_address,
+                            self.model.note,
+                            self.model.create_date,
+                            self.model.update_date,
+                            self.status.name,
+                        )
+                        .select_from(self.user)
+                        .join(self.model, self.model.user_id == self.user.id)
+                        .join(self.status, self.status.id == self.model.status_id)
+                        .where(self.user.email == user_email)
+                    )
+                )
+                .mappings()
+                .all()
+            )
+        ]
--- app/context/order/services/order_service.py
+++ app/context/order/services/order_service.py
@@ -1,6 +1,10 @@
 from app.context.order.dependencies.repositories import IOrderRepository
+from app.context.order.schemas.order_schema import UserOrderDTO
 
 
 class OrderService:
     def __init__(self: "OrderService", repository: IOrderRepository) -> None:
         self.repository = repository
+
+    async def get_user_order_by_email(self: "OrderService", user_email: str) -> list[UserOrderDTO]:
+        return await self.repository.get_user_order_by_email(user_email=user_email)
