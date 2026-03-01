d0024e16a60bc0f050549e95e3be63117b4d39ee
Alina <beketova2007@bk.ru>
2026-03-01 12:53:24 +0300

    add methods genre

--- app/context/genre/controllers/genre_controller.py
+++ app/context/genre/controllers/genre_controller.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
 
+from app.context.genre.dependencies.services import IGenreService
+from app.context.genre.schemas.genre_schema import GenreDTO
+from app.exception_handler import error_handler
+
 router_genre = APIRouter(tags=["genre"])
+
+
+@router_genre.get("/genre", summary="Get book")
+@error_handler
+async def get_genre(service: IGenreService) -> list[GenreDTO]:
+    return await service.get_genre()
